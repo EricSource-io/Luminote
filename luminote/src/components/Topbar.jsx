e11750a294d6688230f5d8cdf1a315588ccd0f06
EricSource <57870666+EricSource2002@users.noreply.github.com>
2023-11-15 00:43:45 +0100

    a

--- luminote/src/components/Topbar.jsx
+++ luminote/src/components/Topbar.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import {
+    IconoirProvider, Bell, MoreHoriz, Enlarge
+} from 'iconoir-react';
 function Topbar () {
     return (
         <div className='topbar'>
-            <button className='text-button'>Home</button>
-            <button className='text-button'>Insert</button>
-            <button className='text-button'>Draw</button>
-            <button className='text-button'>View</button>
+            <div className='topbar-header'>
+                <div className='topbar-left'>
+                    <button className='text-button selected'>Home</button>
+                    <button className='text-button'>Insert</button>
+                    <button className='text-button'>Draw</button>
+                    <button className='text-button'>View</button>
+                </div>
+                <div className='topbar-right'>
+                    <IconoirProvider
+                        iconProps={{
+                            strokeWidth: 2,
+                            width: '1.3rem',
+                            height: '1.3rem',
+                        }}
+                    >
+                        <Bell />
+                        <Enlarge />
+                        <MoreHoriz />
+                    </IconoirProvider>
+                </div>
+            </div>
+            <div className='topbar-content'>
+                        
+            </div>
+            
         </div>);
 }
 
