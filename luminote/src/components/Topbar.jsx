9bd11abfc746df6ab53785b0f42fa9e9c5e1679b
EricSource <57870666+EricSource2002@users.noreply.github.com>
2023-11-22 02:28:58 +0100

    Topbar UI update

--- luminote/src/components/Topbar.jsx
+++ luminote/src/components/Topbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
-    IconoirProvider, Bell, MoreHoriz, Enlarge
+    IconoirProvider, Bell, MoreHoriz, Enlarge, PasteClipboard, Bold, Italic, Underline
 } from 'iconoir-react';
 function Topbar () {
+    const HomeContent = (<>
+        <PasteClipboard />
+        <div className='font-settings'>
+            <div className='input-section'>
+                <input className='font-name' value={'Calibri'} />
+                <input className='font-size' value={'14'} />
+            </div>
+            <div className='action-section'>
+                <IconoirProvider
+                    iconProps={{
+                        strokeWidth: 2,
+                        width: '1.5rem',
+                        height: '1.5rem',
+                    }}
+                >
+                    <button className='icon-button'> <Bold /></button>
+                    <button className='icon-button'> <Italic /></button>
+                    <button className='icon-button'> <Underline /></button>
+                </IconoirProvider>
+            </div>
+        </div>
+
+    </>)
     return (
         <div className='topbar'>
             <div className='topbar-header'>
@@ -28,9 +51,9 @@ function Topbar () {
                 </div>
             </div>
             <div className='topbar-content'>
-                        
+                {HomeContent}
             </div>
-            
+
         </div>);
 }
 
