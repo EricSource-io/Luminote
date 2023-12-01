a48f8190c3500f4c5ae57c0e39740b500a479c5f
EricSource <57870666+EricSource2002@users.noreply.github.com>
2023-12-01 02:59:59 +0100

    TextArea removed

--- luminote/src/components/Canvas.jsx
+++ luminote/src/components/Canvas.jsx
@@ -1,52 +1,12 @@
 import React, { useRef, useState, useEffect } from 'react';
-import TextArea from './TextArea.jsx';
 
-function Canvas() {
+
+function Canvas () {
   const canvasRef = useRef(null);
-  const [textAreas, setTextAreas] = useState([]);
-  const [clickPosition, setClickPosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const handleDoubleClick = (event) => {
-      const clickX = event.clientX - canvasRef.current.offsetLeft;
-      const clickY = event.clientY - canvasRef.current.offsetTop;
-
-      // Check if the click is inside an existing TextArea
-      const isInsideExistingTextArea = textAreas.some(
-        (area) =>
-          clickX >= area.x &&
-          clickX <= area.x + area.width &&
-          clickY >= area.y &&
-          clickY <= area.y + area.height
-      );
-
-      if (!isInsideExistingTextArea) {
-        // Create a new TextArea at the click position
-        const newTextArea = {
-            left: clickX,
-            top: clickY,
-            width: 150, 
-          };
-
-        setTextAreas((prevTextAreas) => [...prevTextAreas, newTextArea]);
-      }
-    };
-
-    // Attach the double-click event listener to the canvas
-    const canvasElement = canvasRef.current;
-    canvasElement.addEventListener('dblclick', handleDoubleClick);
-
-    // Cleanup the event listener when the component is unmounted
-    return () => {
-      canvasElement.removeEventListener('dblclick', handleDoubleClick);
-    };
-  }, [textAreas]);
 
   return (
     <div className='canvas' ref={canvasRef}>
-      {textAreas.map((area, index) => (
-        <TextArea key={index} initialArea={area} />
-      ))}
+
     </div>
   );
 }
--- luminote/src/components/Topbar.jsx
+++ luminote/src/components/Topbar.jsx
@@ -1,40 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
     IconoirProvider, Bell, MoreHoriz, Enlarge, PasteClipboard, Bold, Italic, Underline
 } from 'iconoir-react';
 function Topbar () {
-    const HomeContent = (<>
-        <PasteClipboard />
-        <div className='font-settings'>
-            <div className='input-section'>
-                <input className='font-name' value={'Calibri'} />
-                <input className='font-size' value={'14'} />
-            </div>
-            <div className='action-section'>
-                <IconoirProvider
-                    iconProps={{
-                        strokeWidth: 1.5,
-                        width: '1.5rem',
-                        height: '1.5rem',
-                    }}
-                >
-                    <button className='icon-button'> <Bold strokeWidth={2}/></button>
-                    <button className='icon-button'> <Italic /></button>
-                    <button className='icon-button'> <Underline /></button>
-                </IconoirProvider>
-            </div>
-        </div>
+    const [activeTab, setActiveTab] = useState('home');
 
-    </>)
+    const handleTabChange = (tab) => {
+        setActiveTab(tab);
+    };
+
+    const renderContent = () => {
+        switch (activeTab) {
+            case 'home':
+                return (
+                    <>
+                        <PasteClipboard />
+                        <div className='font-settings'>
+                            <div className='input-section'>
+                                <input className='font-name' value={'Calibri'} />
+                                <input className='font-size' value={'14'} />
+                            </div>
+                            <div className='action-section'>
+                                <IconoirProvider
+                                    iconProps={{
+                                        strokeWidth: 1.5,
+                                        width: '1.5rem',
+                                        height: '1.5rem',
+                                    }}
+                                >
+                                    <button className='icon-button'> <Bold strokeWidth={2} /></button>
+                                    <button className='icon-button'> <Italic /></button>
+                                    <button className='icon-button'> <Underline /></button>
+                                </IconoirProvider>
+                            </div>
+                        </div>
+                    </>
+                );
+            case 'insert':
+                return (
+                    <></>
+                );
+            case 'draw':
+                return (
+                    <>
+                        <button className=''>Pen</button>
+                    </>
+
+                );
+            case 'view':
+                return (
+                    <></>
+                );
+            default:
+                return null;
+        }
+    };
+
     return (
         <div className='topbar'>
             <div className='topbar-header'>
                 <div className='topbar-left'>
-                    <button className='text-button selected'>Home</button>
-                    <button className='text-button'>Insert</button>
-                    <button className='text-button'>Draw</button>
-                    <button className='text-button'>View</button>
+                    <button className={`text-button ${activeTab === 'home' ? 'selected' : ''}`} onClick={() => handleTabChange('home')}>
+                        Home
+                    </button>
+                    <button className={`text-button ${activeTab === 'insert' ? 'selected' : ''}`} onClick={() => handleTabChange('insert')}>
+                        Insert
+                    </button>
+                    <button className={`text-button ${activeTab === 'draw' ? 'selected' : ''}`} onClick={() => handleTabChange('draw')}>
+                        Draw
+                    </button>
+                    <button className={`text-button ${activeTab === 'view' ? 'selected' : ''}`} onClick={() => handleTabChange('view')}>
+                        View
+                    </button>
                 </div>
                 <div className='topbar-right'>
                     <IconoirProvider
@@ -51,7 +89,7 @@ function Topbar () {
                 </div>
             </div>
             <div className='topbar-content'>
-                {HomeContent}
+                <div className='topbar-content'>{renderContent()}</div>
             </div>
 
         </div>);
